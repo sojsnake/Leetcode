709a6f79609b2c1f940554a84698047ebd2ed3ff
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-02 13:36:44 +0900

    Time: 0 ms (100%), Space: 40.6 MB (99.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
-        int curr = 0, prev = 0;
+        int curr = 0, prev=0;
         for(int num : nums){
             int temp = curr;
             curr = Math.max(curr, prev+num);
