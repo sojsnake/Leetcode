abcf232a1caff3984e32fb32b78120874034a837
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-03 15:22:44 +0900

    Time: 0 ms (100%), Space: 40.8 MB (88.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,11 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int curr=0, prev=0;
+        int curr = 0, prev = 0;
         for(int num : nums){
             int temp = curr;
-            curr=Math.max(curr, prev+num);
+            curr = Math.max(curr, prev+num);
             prev = temp;
         }
         return curr;
     }
+
 }
